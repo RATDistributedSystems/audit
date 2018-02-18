69d9eaaf4c3192f070111df187308634a29e5bd7
Rafael Solorzano <rafa@solorzanoc.com>
2018-02-18 02:14:09 -0800

    modify dockerfile, build executable on host system instead of docker
    Copy config.json to get system to run, build the executable directly in the host system instead of inside another docker container added target system of linux so it runs.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM scratch
 
 COPY audit /app/
+COPY config.json /app/
 WORKDIR "/app"
 EXPOSE 44444
 CMD ["./audit"]
--- setup_dockerfile/setup_image.sh
+++ setup_dockerfile/setup_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.9 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o audit'
+CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o audit
 
 # Build the image
 docker build -t auditserver .
